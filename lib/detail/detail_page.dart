0a0ae3139fc1cc0c2de91df90bd2f4d42d5ca3cc
Anderson Lima <andersonlimahw@gmail.com>
2021-10-03 08:06:18 -0300

    Adicionando botão de compartilhar

--- lib/detail/detail_page.dart
+++ lib/detail/detail_page.dart
@@ -1,11 +1,9 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:lemonstarwars/core/app_gradients.dart';
 import 'package:lemonstarwars/core/core.dart';
 import 'package:lemonstarwars/detail/widgets/movie_detail_widget.dart';
 import 'package:lemonstarwars/shared/widgets/app_bar_widget.dart';
-import 'package:lemonstarwars/shared/widgets/movie_card_widget.dart';
+import 'package:share_plus/share_plus.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({Key? key}) : super(key: key);
@@ -23,6 +21,20 @@ class _DetailPageState extends State<DetailPage> {
       //Notes button:
       if(index == 0) {
         print("_onItemTapped $index, TODO: Share movie informations");
+        Share.share(
+          "Olá, olha oque eu encontrei desse filme da franquia Star Wars"+
+          "\n\nTítulo oficial"+
+          "\nA New Hope"+
+          "\n\nDiretores"+
+          "\n..."+
+          "\n\nProdutores"+
+          "\n..."+
+          "\n\nData de lançamento"+
+          "\n..."+          
+          "\n\nResumo"+
+          "\n...",
+          subject: "Star Wars | A New Hope"
+        );
       }
       if(index == 1) {
         print("_onItemTapped $index, TODO: Notes");
@@ -49,17 +61,20 @@ class _DetailPageState extends State<DetailPage> {
             )
           ]),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(icon: Icon(Icons.share), label: ""),
-            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ""),
-            BottomNavigationBarItem(icon: Icon(Icons.arrow_back_ios), label: ""),
-          ],
-          selectedItemColor: AppColors.primaryColorDark,           
-          unselectedItemColor: AppColors.secondaryTextColor,
-          onTap: _onItemTapped,
-          backgroundColor: AppColors.primaryTextColor,
-          iconSize: 18,
+        bottomNavigationBar: SafeArea(
+          bottom: true,
+          child: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(icon: Icon(Icons.share), label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.arrow_back_ios), label: ""),
+            ],
+            selectedItemColor: AppColors.primaryColorDark,           
+            unselectedItemColor: AppColors.secondaryTextColor,
+            onTap: _onItemTapped,
+            backgroundColor: AppColors.primaryTextColor,
+            iconSize: 18,
+          ),
         )
       );
   }
