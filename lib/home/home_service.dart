48da776fd01f64de440092c3547cb4f88e5ecc08
Anderson Lima <andersonlimahw@gmail.com>
2021-10-05 10:53:47 -0300

    Adicionando chamada http na home

--- lib/home/home_service.dart
+++ lib/home/home_service.dart
@@ -1,16 +1,21 @@
+import 'dart:convert';
+
 import 'package:lemonstarwars/shared/enums/e_request_type.dart';
 import 'package:lemonstarwars/shared/models/movie_model.dart';
 import 'package:lemonstarwars/shared/services/base_service.dart';
 
 class HomeService {
-  // Future<List<MovieModel>> getMovies() async {
-  Future<bool> getMovies() async {
+  Future<List<MovieModel>> getMovies() async {
     print("HomeService:getMovies(), Loading");
+
     final url = "films";
     final service = await BaseService(url: url, method: ERquestType.Get);
-    final response = await service.get();
-    // final movies = List.filled(1, MovieModel); // TODO mapper json
-    print("HomeService:getMovies(), Success, response ${response.statusCode}");
-    return response.statusCode == 200;
+    final response = await service.get();    
+    final body = jsonDecode(response.body);    
+    final results = body['results'] as List;   
+    final movies = results.map((movie) => MovieModel.fromMap(movie)).toList();
+    
+    print("HomeService:getMovies(), Success, response ${response.statusCode}, Count: ${movies.length} ");
+    return movies;
   }  
 }
--- lib/shared/models/movie_model.dart
+++ lib/shared/models/movie_model.dart
@@ -1,6 +1,101 @@
-class MovieModel {
-  final String title;
+import 'dart:convert';
+import 'dart:core';
 
-  MovieModel( { required this.title });
+class MovieModel {
+  final int episode_id;
+  final String title;
+  final String director;
+  final String opening_crawl;
+  final String producer;
+  final String release_date;
+  final String url;
   
+  MovieModel({
+    required this.episode_id,
+    required this.title,
+    required this.director,
+    required this.opening_crawl,
+    required this.producer,
+    required this.release_date,
+    required this.url,
+  });
+
+
+  Map<String, dynamic> toMap() {
+    return {
+      'episode_id': episode_id,
+      'title': title,
+      'director': director,
+      'opening_crawl': opening_crawl,
+      'producer': producer,
+      'release_date': release_date,
+      'url': url,
+    };
+  }
+
+  factory MovieModel.fromMap(Map<String, dynamic> map) {
+    return MovieModel(
+      episode_id: map['episode_id'],
+      title: map['title'],
+      director: map['director'],
+      opening_crawl: map['opening_crawl'],
+      producer: map['producer'],
+      release_date: map['release_date'],
+      url: map['url'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory MovieModel.fromJson(String source) => MovieModel.fromMap(json.decode(source));
+
+  MovieModel copyWith({
+    int? episode_id,
+    String? title,
+    String? director,
+    String? opening_crawl,
+    String? producer,
+    String? release_date,
+    String? url,
+  }) {
+    return MovieModel(
+      episode_id: episode_id ?? this.episode_id,
+      title: title ?? this.title,
+      director: director ?? this.director,
+      opening_crawl: opening_crawl ?? this.opening_crawl,
+      producer: producer ?? this.producer,
+      release_date: release_date ?? this.release_date,
+      url: url ?? this.url,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'MovieModel(episode_id: $episode_id, title: $title, director: $director, opening_crawl: $opening_crawl, producer: $producer, release_date: $release_date, url: $url)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is MovieModel &&
+      other.episode_id == episode_id &&
+      other.title == title &&
+      other.director == director &&
+      other.opening_crawl == opening_crawl &&
+      other.producer == producer &&
+      other.release_date == release_date &&
+      other.url == url;
+  }
+
+  @override
+  int get hashCode {
+    return episode_id.hashCode ^
+      title.hashCode ^
+      director.hashCode ^
+      opening_crawl.hashCode ^
+      producer.hashCode ^
+      release_date.hashCode ^
+      url.hashCode;
+  }
 }
