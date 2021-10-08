101f975670c1b836c8d59c455bf0c493d29816f2
Anderson Lima <andersonlimahw@gmail.com>
2021-10-07 22:39:47 -0300

    Refatorando home_service para herdar de base_service

--- lib/home/home_service.dart
+++ lib/home/home_service.dart
@@ -4,21 +4,20 @@ import 'package:lemonstarwars/shared/enums/e_request_type.dart';
 import 'package:lemonstarwars/shared/models/movie_model.dart';
 import 'package:lemonstarwars/shared/services/base_service.dart';
 
-class HomeService {
+class HomeService extends BaseService {
+  HomeService() : super(url: 'films', method: ERequestType.Get);
   /**
    * Return a List of Movies
    */
   Future<List<MovieModel>> getMovies() async {
     print("HomeService:getMovies(), Loading");
-    // Request:
-    final url = "films";
-    final service = await BaseService(url: url, method: ERequestType.Get);
-    final response = await service.get();    
-    
+    // Request:    
+    final response = await this.get().then((value) => value);    
     // Translation:
     final body = jsonDecode(response.body);    
     final results = body['results'] as List;   
     final movies = results.map((movie) => MovieModel.fromMap(movie)).toList();
+    
     // Order by Epsisode
     movies.sort((a, b) => a.episode_id.compareTo(b.episode_id));    
 
